7d9b4994a27de53a479399e8eca3f4ade9135422
Jason K. <phantomtypist@hotmail.com>
2014-02-03 10:02:19 -0500

    Added model for "location" entity.

---
+++ models/location.js
@@ -0,0 +1,16 @@
+﻿var uuid = require("node-uuid");
+var assert = require("assert");
+
+var Location = function(args) {
+    args = args || {};
+    
+    assert.ok(args.name && args.description, "Need a name and description");
+
+    this.uuid = args.uuid || uuid.v1();
+    this.name = args.name;
+    this.description = args.description;
+
+    return this;
+};
+
+module.exports = Location;
