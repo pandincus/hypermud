c60ccc80a0f8e0513a2ba7f77bc2b4c3556b1145
Jason K. <phantomtypist@hotmail.com>
2014-02-03 10:29:15 -0500

    - Modified index controller and model a little bit to set things up a bit better. - Also included some files and folders in the Visual Studio project.

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-
+var IndexModel = require('../models/index');
 module.exports = function (server) {
-
+    var model = new IndexModel();
+    
     server.get('/', function (req, res) {
-        var model = { name: 'hypermud' };
+        model.name = 'Dan and Jason';
         
         res.render('index', model);
         
